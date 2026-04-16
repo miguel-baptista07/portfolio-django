f843597898e965bd2d35c45d862a6e75d98bdce8
Miguel Baptista <a22405192@alunos.ulht.pt>
2026-04-16 08:30:02 +0000

    Portfolio: views, urls, templates e estilos criados

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Licenciatura, UnidadeCurricular, Projeto, Tecnologia, TFC, Competencia, Formacao
 
-# Create your views here.
+
+def licenciaturas_view(request):
+    licenciaturas = Licenciatura.objects.all()
+    return render(request, 'portfolio/licenciaturas.html', {'licenciaturas': licenciaturas})
+
+
+def ucs_view(request):
+    ucs = UnidadeCurricular.objects.select_related('licenciatura').prefetch_related('docentes').all()
+    return render(request, 'portfolio/ucs.html', {'ucs': ucs})
+
+
+def projetos_view(request):
+    projetos = Projeto.objects.select_related('uc').prefetch_related('tecnologias').all()
+    return render(request, 'portfolio/projetos.html', {'projetos': projetos})
+
+
+def tecnologias_view(request):
+    tecnologias = Tecnologia.objects.all()
+    return render(request, 'portfolio/tecnologias.html', {'tecnologias': tecnologias})
+
+
+def tfcs_view(request):
+    tfcs = TFC.objects.all()
+    return render(request, 'portfolio/tfcs.html', {'tfcs': tfcs})
+
+
+def competencias_view(request):
+    competencias = Competencia.objects.prefetch_related('tecnologias', 'projetos').all()
+    return render(request, 'portfolio/competencias.html', {'competencias': competencias})
+
+
+def formacoes_view(request):
+    formacoes = Formacao.objects.all()
+    return render(request, 'portfolio/formacoes.html', {'formacoes': formacoes})
