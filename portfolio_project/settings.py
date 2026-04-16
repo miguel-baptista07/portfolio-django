99da3047b3a3b48ac89b6dd03c0176ca724cbcf2
Miguel Baptista <a22405192@alunos.ulht.pt>
2026-04-16 08:12:57 +0000

    Escola: whitenoise configurado e fix no template de cursos sem imagem

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (uploads)
 MEDIA_ROOT = BASE_DIR / 'media'
