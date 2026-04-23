141e4d8d5a68c2cddd5780af561e669f3ac0209d
Miguel Baptista <miguelkevlin23@gmail.com>
2026-04-23 09:37:30 +0100

    portfolio: rotas CRUD adicionadas em urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -10,4 +10,24 @@ urlpatterns = [
     path('competencias/', views.competencias_view, name='competencias'),
     path('formacoes/', views.formacoes_view, name='formacoes'),
     path('', views.projetos_view),
+
+    path('projeto/<int:id>/', views.projeto_view, name='projeto'),
+    path('projeto/novo/', views.novo_projeto_view, name='novo_projeto'),
+    path('projeto/<int:id>/edita/', views.edita_projeto_view, name='edita_projeto'),
+    path('projeto/<int:id>/apaga/', views.apaga_projeto_view, name='apaga_projeto'),
+
+    path('tecnologia/<int:id>/', views.tecnologia_view, name='tecnologia'),
+    path('tecnologia/novo/', views.nova_tecnologia_view, name='nova_tecnologia'),
+    path('tecnologia/<int:id>/edita/', views.edita_tecnologia_view, name='edita_tecnologia'),
+    path('tecnologia/<int:id>/apaga/', views.apaga_tecnologia_view, name='apaga_tecnologia'),
+
+    path('competencia/<int:id>/', views.competencia_view, name='competencia'),
+    path('competencia/novo/', views.nova_competencia_view, name='nova_competencia'),
+    path('competencia/<int:id>/edita/', views.edita_competencia_view, name='edita_competencia'),
+    path('competencia/<int:id>/apaga/', views.apaga_competencia_view, name='apaga_competencia'),
+
+    path('formacao/<int:id>/', views.formacao_view, name='formacao'),
+    path('formacao/novo/', views.nova_formacao_view, name='nova_formacao'),
+    path('formacao/<int:id>/edita/', views.edita_formacao_view, name='edita_formacao'),
+    path('formacao/<int:id>/apaga/', views.apaga_formacao_view, name='apaga_formacao'),
 ]
